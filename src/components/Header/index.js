d922b5edda523596719d01bd500a718ace64a396
Lívia <liviamarcelasouza@hotmail.com>
2023-01-30 21:50:49 -0300

    Initial Project

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+  return (
+    <header>
+        {/* logo */}
+        <img />
+      <span>PHF</span>
+      <h2>lorem</h2>
+      {/* unsplash */}
+      <img /> 
+    </header>
+  );
+};
+
+export default Header;
