4a67a8e2f3e3a68b40b0ee4a0bc150c5e0f04372
Gabriel dos Santos <gabriel_sspirangi@hotmail.com>
2023-02-04 23:22:29 -0300

    - Cards criados e com o estilo corrigido; - Carrocel criado; - Imagens dos Cards e do Carrocel ainda ausentes.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Sections/sectionHome";
 import Cards from "../src/components/Cards/CardWork/index";
+import BasicExample from "./components/Cards/CardServices";
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
-      <Cards />
+      <BasicExample />
     </div>
   );
 }
--- src/components/Sections/sectionHome.js
+++ src/components/Sections/sectionHome.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
 
 import styled from "styled-components";
 import background from "../../image/engenharia2.jpg";
@@ -25,6 +26,7 @@ const TextContentPrimary = styled.p`
 
 function Home() {
     return(
+        <div>
         <BodyHome style={{backgroundImage: `url(${background})`,
                         backgroundSize: "cover",
                         backgroundRepeat: "no-repeat",
@@ -35,6 +37,49 @@ function Home() {
         <TextContentPrimary>Nossa empresa é especializada na confecção de plantas
         das mais diversas formas que você pode imaginar.</TextContentPrimary>
         </BodyHome>
+
+        <div>
+        <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        </div>
+        </div>
 
         
         
